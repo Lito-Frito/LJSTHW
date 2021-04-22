e35925f7db2ba0e124c8686653c29e9a61181781
crc8109 <crc8109@pm.me>
2021-04-22 09:00:54 -0400

    Add ex14

---
+++ ex14.js
@@ -0,0 +1,10 @@
+  // copy and paste these into node to evaluate and test logic
+5 < 10 && 7 > 9 // False
+3 > '5' // False
+21 === '21' // False
+78 <= 1023 && 45 > 23 || 34 < 23 // True
+7 >= 3 && 94 == 23 && 45 < 100 || 89 == 89 // True
+6 + 34 < 2 * 20 && 78 - 23 > 100 - 30 // False
+2 + 3 * 67 <= 789 // True
+let x = 45; // True
+5 + x < x && x * 20 > 34 * 2 // False
