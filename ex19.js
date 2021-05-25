381b631956424a7f176dd23315326395c0eae733
crc8109 <crc8109@pm.me>
2021-05-24 22:06:05 -0400

    Add ex19

---
+++ ex19.js
@@ -0,0 +1,65 @@
+// Run this command in your terminal first to check answers:
+// let data = require('./ex19');
+
+exports.fruit = [
+    {kind: 'Apples',  count: 12, rating: 'AAA'},
+    {kind: 'Oranges', count: 1,  rating: 'B'},
+    {kind: 'Pears',   count: 2,  rating: 'A'},
+    {kind: 'Grapes',  count: 14, rating: 'UR'}
+];
+
+exports.cars = [
+    {type: 'Cadillac', color: 'Black', size: 'Big', miles: 34500},
+    {type: 'Corvette', color: 'Red', size: 'Little', miles: 1000000},
+    {type: 'Ford', color: 'Blue', size: 'Medium', miles: 1234},
+    {type: 'BMW', color: 'White', size: 'Baby', miles: 7890}
+];
+
+exports.languages = [
+    {name: 'Python', speed: 'Slow', opinion: ['Terrible', 'Mush']},
+    {name: 'JavaScript', speed: 'Moderate', opinion: ['Alright', 'Bizarre']},
+    {name: 'Perl6', speed: 'Moderate', opinion: ['Fun', 'Weird']},
+    {name: 'C', speed: 'Fast', opinion: ['Annoying', 'Dangerous']},
+    {name: 'Forth', speed: 'Fast', opinion: ['Fun', 'Difficult']},
+];
+
+
+// Answers:
+// Fruit Challenge
+// data.fruit[0].count
+// data.fruit[0].rating
+// data.fruit[1].kind
+// data.fruit[3].kind
+// data.fruit[3].count
+// data.fruit[0].kind
+// -----------------------
+// Cars Challenge
+// data.cars[0].size
+// data.cars[1].color
+// data.cars[2].miles
+// data.cars[3].color
+// data.cars[3].miles
+// data.cars[0].color
+// data.cars[0].miles
+// data.cars[2].color
+// -----------------------
+// Languages Challenge
+// data.languages[0].speed
+// data.languages[1].opinion[0]
+// data.languages[3].opinion[1]
+// data.languages[3].speed
+// data.languages[4].opinion[1]
+// data.languages[2].opinion[0]
+// data.languages[3].opinion[0]
+// data.languages[2].opinion[1]
+// data.languages[2].speed
+// -----------------------
+// FINAL CHALLENGE
+// data.cars[1].size
+// data.cars[1].color
+// data.cars[1].type
+// data.cars[3].size
+// data.fruit[3].rating
+// data.languages[0].opinion[1]
+// data.fruit[2].count
+// data.languages[3].speed
