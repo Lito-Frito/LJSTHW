890b167c40ae405ab5cc7fd7b99ffd2394a36aae
crc8109 <crc8109@pm.me>
2021-05-26 11:52:54 -0400

    Add new paths to game

--- ex20.js
+++ ex20.js
@@ -1,15 +1,15 @@
 const readline = require('readline-sync');
 
-const say = (prompt) => {
+const say = (prompt) => { // echos a prompt
     console.log(prompt);
 }
 
-const die = (message) => {
+const die = (message) => { // returns a death message and exits game
     say(message);
     process.exit(1);
 }
 
-const ask = (hp, prompt) => {
+const ask = (hp, prompt) => { // shows your HP and asks you what to do (prompt)
     console.log(`[[You have ${hp} hit points.]]`);
     if(hp <= 0) {
         die("You died!");
@@ -33,15 +33,35 @@ const gold = (hp) => {
 }
 
 const rope = (hp) => {
+  say("You see a door at the bottom of the well.");
+  say("There's also a window you can go through.");
+    let ropeChoice = ask(hp,"What do you do?");
     // they are at the bottom of the well
     // they can go through the door to the gold
     // or go take a wrong turn to the spider
 }
+const greeting = () => { // Greets player and gets name
+  say(`Welcome to "${process.argv[1]}" world!`)
+  let name = readline.question("What's your name? ");
+  say(`Welcome ${name}!`)
+
+  let adventure = readline.question("Shall we adventure (Y/N)? ");
+
+  if (adventure === "Y") {
+      well(hp);
+    } else if (adventure === "N") {
+          die("You wander around and eventually starve to death!");
+    } else {
+          say("You can't do that here.");
+          die("You wander around and eventually starve to death!");
+    }
+
+}
 
 const well = (hp) => {
-    say("You are walking through the woods and see a well.");
-    say("Walking up to it and looking down you see a shiny thing at the bottom.");
-    let next = ask(hp, "What do you do?");
+  say("You are walking through the woods and see a well.");
+  say("Walking up to it and looking down you see a shiny thing at the bottom.");
+  let next = ask(hp, "What do you do?");
 
     if(next === "climb") {
         say("You climb down the rope.");
@@ -60,4 +80,4 @@ const well = (hp) => {
 let hp = Math.floor(Math.random() * 10) + 1;
 
 // this starts the game
-well(hp)
+greeting()
