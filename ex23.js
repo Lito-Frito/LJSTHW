22cbdf6dfdb9312ddaf7d7a3cd8270bcacdf121c
crc8109 <crc8109@pm.me>
2021-06-03 15:06:31 -0400

    Add ex23

---
+++ ex23.js
@@ -0,0 +1,15 @@
+class Guitar {
+    play(note) {
+        // code for playing
+    }
+}
+
+class Telecaster extends Guitar {
+    volume(level) {
+        // code for volume
+    }
+
+    tone(level) {
+        // code for tone
+    }
+}
